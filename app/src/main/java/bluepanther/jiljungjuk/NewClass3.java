f4239d0ebb49e5d4109d7cf37e01da9d9e68a52b
shyam <shyamsuganth95@gmail.com>
2017-02-21 16:33:09 +0530

    changin audiodesc

--- app/src/main/java/bluepanther/jiljungjuk/NewClass3.java
+++
@@ -1,8 +0,0 @@
-package bluepanther.jiljungjuk;
-
-/**
- * Created by shyam on 21/2/17.
- */
-
-public class NewClass3 {
-}
--- app/src/main/java/bluepanther/jiljungjuk/NewClass_2.java
+++
@@ -1,8 +0,0 @@
-package bluepanther.jiljungjuk;
-
-/**
- * Created by hhs on 21/2/17.
- */
-
-public class NewClass_2 {
-}
